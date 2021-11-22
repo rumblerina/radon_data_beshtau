9f1bdde2fed397bc7bdc10e10f4ef0fddd72da26
rumblerina <47294596+rumblerina@users.noreply.github.com>
2021-11-22 19:56:37 +0300

    Update tensorRadonBesht.py
    Added prediction-making algorithm based on the same meteorological data as input.
    Prediction data is smoothed using Savitzky-Golay algorithm.

--- tensorRadonBesht.py
+++ tensorRadonBesht.py
@@ -1,27 +1,37 @@
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import matplotlib.cbook as cbook
+from matplotlib.dates import DateFormatter
 import numpy as np
+from numpy.core.arrayprint import DatetimeFormat
 from numpy.lib.histograms import _ravel_and_check_weights
 import pandas as pd
+from pandas.io.formats.format import Datetime64Formatter
 import seaborn as sns
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.layers.experimental import preprocessing
+import warnings
+import datetime as dt
+warnings.simplefilter(action='ignore', category=FutureWarning)
+from pandas.plotting import register_matplotlib_converters
+register_matplotlib_converters()
 
 #dataset
-#weather data obtained from the open weather archive rp5.ru
 column_names = ['datetime', 'ur/h', 'usv/h', 't', 'p0', 'humid', 'windspd', 'Td', 'rrr', 'mmprecip24', 'mmprecip48']
 raw_dataset = pd.read_csv("beshtaugamma.csv", names = column_names, na_values='?', sep =',', skiprows = 1)
 dataset = raw_dataset.copy()
-dataset = dataset.drop('datetime', 1)
 dataset = dataset.drop('ur/h', 1)
 dataset = dataset.dropna()
-print(dataset)
+timestamps_orig = dataset.pop('datetime')
+#print(pred_set)
 
 #split into test and train sets:
 train_dataset = dataset.sample(frac = 0.8, random_state = 0)
 test_dataset = dataset.drop(train_dataset.index)
 print(train_dataset)
+print(test_dataset)
 
 #split features from labels, label - value we're looking for
 train_features = train_dataset.copy()
@@ -52,12 +62,13 @@ def build_and_compile_model(norm):
     model = keras.Sequential([
         norm,
         layers.Dense(256, activation='relu'),
-        layers.Dense(256, activation='relu'),
         layers.Dense(1)
     ])
     model.compile(loss='mean_absolute_error',
                 optimizer=tf.keras.optimizers.Adam(0.001))
     return model
+
+callback = tf.keras.callbacks.EarlyStopping(monitor = 'val_loss', patience = 20)
 
 #Full DNN model
 dnn_model = build_and_compile_model(normalizer)
@@ -66,10 +77,11 @@ history = dnn_model.fit(
     train_features, train_labels,
     batch_size = 256,
     validation_split=0.2,
-    verbose=0, epochs=5000)
+    verbose=1, epochs=1000,
+    callbacks = [callback])
 
-plot_loss(history)
-plt.show()
+# plot_loss(history)
+# plt.show()
 
 #Predictions
 test_predictions = dnn_model.predict(test_features).flatten()
@@ -77,10 +89,6 @@ a = plt.axes(aspect='equal')
 plt.scatter(test_labels, test_predictions)
 plt.xlabel('True Values [usv/h]')
 plt.ylabel('Predictions [usv/h]')
-lims = [0, 1]
-plt.xlim(lims)
-plt.ylim(lims)
-_ = plt.plot(lims, lims)
 plt.show()
 
 #Error distribution for predictions
@@ -89,3 +97,58 @@ plt.hist(error, bins=25)
 plt.xlabel('Prediction Error [usv/h]')
 _ = plt.ylabel('Count')
 plt.show()
+
+#Show original plot
+y_orig = dataset['usv/h']
+plt.plot(timestamps_orig,y_orig)
+plt.gcf().autofmt_xdate()
+plt.xticks(timestamps_orig[::56])
+plt.xlabel("Date")
+plt.ylabel("Dose rate, usv/h")
+plt.title("Actual doserate at mt. Beshtau in 2018-19")
+plt.show()
+ 
+#define the savitzky-golay smoothing algorithm
+def savitzky_golay(y, window_size, order, deriv=0, rate=1):
+    import numpy as np
+    from math import factorial
+    try:
+        window_size = np.abs(np.int(window_size))
+        order = np.abs(np.int(order))
+    except ValueError as msg:
+        raise ValueError('window_size and order have to be type int')
+    if window_size % 2 != 1 or window_size < 1:
+        raise TypeError('Window size must be a positive odd number')
+    if window_size < order + 2:
+        raise TypeError('Window_size is too small for the polynomials order')
+    order_range = range(order+1)
+    half_window = (window_size - 1) // 2
+    #precompute coefficients
+    b = np.mat([[k**i for i in order_range] for k in range(-half_window, half_window+1)])
+    m = np.linalg.pinv(b).A[deriv] * rate ** deriv * factorial(deriv)
+    firstvals = y[0] - np.abs(y[1:half_window+1][::-1] - y[0])
+    lastvals = y[-1] + np.abs(y[-half_window:-1][::-1] - y[-1])
+    y = np.concatenate((firstvals, y, lastvals))
+    return np.convolve(m[::-1], y, mode = 'valid')
+
+#Make predictions for given data
+p_column_names = ['datetime', 't', 'p0', 'humid', 'windspd', 'Td', 'rrr', 'mmprecip24', 'mmprecip48']
+ds_pred = pd.read_csv("beshtaugamma2020.csv", names = p_column_names, na_values='?', sep =',', skiprows = 1)
+timestamps = ds_pred.pop('datetime')
+predix = dnn_model.predict(ds_pred).flatten()
+timestamps = timestamps[:-1]
+pred_smoothed = savitzky_golay(predix, 25, 2)
+predix = predix[:-1]
+#This is the unsmoothed plot
+#plt.plot(timestamps,predix, color = 'red')
+#This is the smoothed plot
+plt.plot(timestamps,pred_smoothed, color = 'blue')
+plt.gcf().autofmt_xdate()
+plt.xticks(timestamps[::56])
+plt.xlabel("Date")
+plt.ylabel("Dose rate, usv/h")
+plt.title('Predicted dose rate at mt. Beshtau in 2019-20')
+plt.show()
+
+#sns.pairplot(raw_dataset, kind = 'reg', plot_kws = {'line_kws':{'color':'blue'}})
+#plt.show()
